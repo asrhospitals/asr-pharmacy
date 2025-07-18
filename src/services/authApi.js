cd746bb3c3a68898d3172c94e9bdc6f8ed429cd6
Parivesh09 <rimjha.parivesh2002@gmail.com>
2025-07-18 19:39:39 +0530

    feat: Update static.yml and authApi.js to include environment variable for backend URL

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,30 +1,31 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
   ? `${import.meta.env.VITE_BACKEND_BASE_URL}/auth`
-  : '/auth';
+  : "/auth";
 
-  console.log("baseUrl", baseUrl);
-  
+console.log("baseUrl", baseUrl);
+console.log("env url", import.meta.env.VITE_BACKEND_BASE_URL);
+
 export const authApi = createApi({
-  reducerPath: 'authApi',
+  reducerPath: "authApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
-        url: '/signin',
-        method: 'POST',
+        url: "/signin",
+        method: "POST",
         body: credentials,
       }),
     }),
     signUp: builder.mutation({
       query: (userData) => ({
-        url: '/sign-up',
-        method: 'POST',
+        url: "/sign-up",
+        method: "POST",
         body: userData,
       }),
     }),
   }),
 });
 
-export const { useLoginMutation, useSignUpMutation } = authApi; 
+export const { useLoginMutation, useSignUpMutation } = authApi;
